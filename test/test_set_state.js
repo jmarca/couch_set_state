ecb673ce20e07f4408607fbe664e5bf19266f202
James E. Marca <james@activimetrics.com>
2014-12-08 15:26:01 -0800

    fixed, tests passing

--- test/test_set_state.js
+++ test/test_set_state.js
@@ -8,19 +8,21 @@ var getter = require('couch_check_state')
 var _ = require('lodash')
 var superagent = require('superagent')
 
-var env = process.env;
-var cuser = env.COUCHDB_USER ;
-var cpass = env.COUCHDB_PASS ;
-var chost = env.COUCHDB_HOST || 'localhost';
-var cport = env.COUCHDB_PORT || 5984;
+var path    = require('path')
+var rootdir = path.normalize(__dirname)
+var config_okay = require('config_okay')
+var config_file = rootdir+'/../test.config.json'
+
+var queue = require('queue-async')
 
 var date = new Date()
-var test_db ='test%2fbulk%2fdeleter_'+date.getSeconds()
-var couch = 'http://'+chost+':'+cport+'/'+test_db
-date = new Date()
 var inprocess_string = date.toISOString()+' inprocess'
 
-var docs = {'docs':[{'_id':'doc1'
+var cdb
+var docs = []
+function populate_db(config,cb){
+
+var mydocs = {'docs':[{'_id':'doc1'
                     ,foo:'bar'}
                    ,{'_id':'doc2'
                     ,'baz':'bat'}
@@ -79,57 +81,69 @@ var docs = {'docs':[{'_id':'doc1'
 
                    ]}
 
+    superagent.post(cdb+'/_bulk_docs')
+    .type('json')
+    .send(mydocs)
+    .end(function(e,r){
+        if(e) cb(e)
+        _.each(r.body
+              ,function(resp){
+                   resp.should.have.property('ok')
+                   docs.push({doc:{_id:resp.id
+                                  ,_rev:resp.rev}})
+               });
+        return cb()
+    })
+    return null
+}
+
+function create_tempdb(config,cb){
+    var date = new Date()
+    var test_db_unique = [date.getHours(),
+                          date.getMinutes(),
+                          date.getSeconds(),
+                          date.getMilliseconds()].join('-')
+    config.couchdb.db += test_db_unique
+    cdb ='http://'+
+        [config.couchdb.host+':'+config.couchdb.port
+        ,config.couchdb.db].join('/')
+    superagent
+    .put(cdb)
+    .auth(config.couchdb.auth.username,
+          config.couchdb.auth.password
+         )
+    .end(function(e,r){
+        should.not.exist(e)
+        return cb()
+    })
+    return null
+}
+var config={}
+
+
 describe('set vds id states',function(){
     //var created_locally=false
     before(function(done){
-        // create a test db, the put data into it
-        var opts = {'uri':couch
-                   ,'method': "PUT"
-                   ,'headers': {}
-                   };
-        opts.headers.authorization = 'Basic ' + new Buffer(cuser + ':' + cpass).toString('base64')
-        opts.headers['Content-Type'] =  'application/json'
-        superagent.put(couch)
-        .auth(cuser,cpass)
-        .end(function(e,r){
-            should.exist(r)
-            //console.log(couch)
-            //console.log(r.error)
-            //if( r.error === undefined ){
-                //created_locally=true
-            // }
-            // now populate that db with some docs
-            superagent.post(couch+'/_bulk_docs')
-            .type('json')
-            .send(docs)
-            .end(function(e,r){
-                if(e) done(e)
-                docs=[]
-                _.each(r.body
-                      ,function(resp){
-                           resp.should.have.property('ok')
-                           docs.push({doc:{_id:resp.id
-                                          ,_rev:resp.rev}})
-                       });
-                return done()
-            })
-            return null
+        config_okay(config_file,function(err,c){
+            should.not.exist(err)
+            config.couchdb = c.couchdb
+            queue(1)
+            .defer(create_tempdb,config)
+            .defer(populate_db,config)
+            .await(done)
         })
+
     })
     after(function(done){
-        //if(!created_locally) return done()
-
-        var couch = 'http://'+chost+':'+cport+'/'+test_db
-        // bail in development
-        //console.log(couch)
-        //return done()
-        var opts = {'uri':couch
+        var opts = {'uri':cdb
                    ,'method': "DELETE"
                    ,'headers': {}
                    };
-        superagent.del(couch)
+        superagent.del(cdb)
         .type('json')
-        .auth(cuser,cpass)
+        .auth(config.couchdb.auth.username,
+              config.couchdb.auth.password
+             )
         .end(function(e,r){
             if(e) return done(e)
             return done()
@@ -139,19 +153,19 @@ describe('set vds id states',function(){
 
     it('should set chain lengths state for doc1, 2007'
       ,function(done){
-           setter({'db':test_db
-                  ,'doc':'801245'
-                  ,'year':2008
-                  ,'state':'truckimputed'
-                  ,'value':inprocess_string
-                  }
+           var task = _.assign({}
+                              ,config.couchdb
+                              ,{'doc':'801245'
+                               ,'year':2008
+                               ,'state':'truckimputed'
+                               ,'value':inprocess_string
+                               })
+
+           setter(task
                  ,function(err,state){
                       should.not.exist(err)
-                      getter({'db':test_db
-                             ,'doc':'801245'
-                             ,'year':2008
-                             ,'state':'truckimputed'
-                             }
+                      delete task.value
+                      getter(task
                             ,function(err,state){
                                  should.not.exist(err)
                                  state.should.eql(inprocess_string)
@@ -161,17 +175,17 @@ describe('set vds id states',function(){
        });
     it('should set inprocess string'
       ,function(done){
-           setter({'db':test_db
-                  ,'doc':'doc1'
-                  ,'year':2008
-                  ,'state':'vdsraw_chain_lengths'
-                  ,'value':[11,23,19,22,15]}
+           var task = _.assign({}
+                              ,config.couchdb
+                              ,{'doc':'doc1'
+                               ,'year':2008
+                               ,'state':'vdsraw_chain_lengths'
+                               ,'value':[11,23,19,22,15]})
+           setter(task
                  ,function(err,state){
                       should.not.exist(err)
-                      getter({'db':test_db
-                             ,'doc':'doc1'
-                             ,'year':2008
-                             ,'state':'vdsraw_chain_lengths'}
+                      delete task.value
+                      getter(task
                             ,function(err,state){
                                  should.not.exist(err)
                                  state.should.have.property('length',5)
@@ -184,33 +198,28 @@ describe('set vds id states',function(){
     it('should remove entirely if variable is defined but null'
       ,function(done){
            // now you see it
-           setter({'db':test_db
-                  ,'doc':'doc1'
-                  ,'year':2008
-                  ,'state':'vdsraw_chain_lengths'
-                  ,'value':[1111,23,19,22,15]}
+           var task = _.assign({}
+                              ,config.couchdb
+                              ,{'doc':'doc1'
+                               ,'year':2008
+                               ,'state':'vdsraw_chain_lengths'
+                               ,'value':[1111,23,19,22,15]})
+           setter(task
                  ,function(err,state){
                       should.not.exist(err)
-                      getter({'db':test_db
-                             ,'doc':'doc1'
-                             ,'year':2008
-                             ,'state':'vdsraw_chain_lengths'}
+                      delete task.value
+                      getter(task
                             ,function(err,state){
                                  should.not.exist(err)
                                  state.should.have.property('length',5)
                                  state.should.eql([1111,23,19,22,15])
                                  // now you don't
-                                 setter({'db':test_db
-                                        ,'doc':'doc1'
-                                        ,'year':2008
-                                        ,'state':'vdsraw_chain_lengths'
-                                        ,'value':null}
+                                 task.value = null
+                                 setter(task
                                        ,function(err,state){
                                             should.not.exist(err)
-                                            getter({'db':test_db
-                                                   ,'doc':'doc1'
-                                                   ,'year':2008
-                                                   ,'state':'vdsraw_chain_lengths'}
+                                            delete task.value
+                                            getter(task
                                                   ,function(err,state){
                                                        should.not.exist(err)
                                                        should.not.exist(state)
@@ -224,25 +233,23 @@ describe('set vds id states',function(){
        });
     it('should create a clean, new document if needed'
       ,function(done){
-   getter({'db':test_db
-          ,'doc':'count_docula'
-          ,'year':2007
-          ,'state':'look_at_the_birdie'}
+           var task = _.assign({}
+                              ,config.couchdb
+                              ,{'doc':'doc1'
+                               ,'year':2008
+                               ,'state':'look_at_the_birdie'})
+
+   getter(task
          ,function(err,state){
               should.not.exist(err)
               should.not.exist(state)
               // now set that value
-              setter({'db':test_db
-                     ,'doc':'count_docula'
-                     ,'year':2007
-                     ,'state':'look_at_the_birdie'
-                     ,'value':'poof'}
+              task.value='poof'
+              setter(task
                     ,function(err){
                          should.not.exist(err)
-                         getter({'db':test_db
-                                ,'doc':'count_docula'
-                                ,'year':2007
-                                ,'state':'look_at_the_birdie'}
+                         delete task.value
+                         getter(task
                                ,function(err,state){
                                     should.not.exist(err)
                                     should.exist(state)
